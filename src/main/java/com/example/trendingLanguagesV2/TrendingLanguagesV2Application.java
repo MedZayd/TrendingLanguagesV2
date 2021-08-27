0d225bb5e8e81ce05192f223f9ae5045ed6b1501
Abad Mohamed Zayd <mohamed.zayd-abad@teal.ma>
2021-08-27 17:19:07 +0100

    project initialization

---
+++ src/main/java/com/example/trendingLanguagesV2/TrendingLanguagesV2Application.java
@@ -0,0 +1,14 @@
+package com.example.trendingLanguagesV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TrendingLanguagesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingLanguagesV2Application.class, args);
+	}
+
+}
